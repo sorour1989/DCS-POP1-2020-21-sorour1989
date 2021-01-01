e18aec286bd77171d82d1a571626e07d3926b5f3
sorour1989 <64619099+sorour1989@users.noreply.github.com>
2021-01-01 14:18:30 +0000

    function ship type defined

--- battleships.py
+++ battleships.py
@@ -12,81 +12,107 @@
 #     _hit = 0     # _ means attribute is private
 #     hit_coords = list()
 #---------------
-import random
-import numpy as np
+# import random
+# import numpy as np
+#
+#
+# def ship():
+#
+#     row = random.randint(0,9)
+#     column = random.randint(0,9)
+#     horizontal = random.choice([True,False])
+#     length = random.randint(1,4)
+#     ships = tuple(np.array([row, column, horizontal, length]))
+#
+#     return ships
+#
+# f= ship()
+# print(f)
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# def fleet
+#
+#
+#
+# def is_sunk(ship):
+#     if len(ship[4]) == ship[3]:
+#         return 'True'
+#     else:
+#         return 'False'
+#
+#
+# #def squares_occupied(ship):
+#
+#
+#
+#
+# def ship_type(ship):
+#     length = [1, 2, 3, 4]
+#     if ship[3] not in length:
+#         return "wrong length"
+#     else:
+#         if ship[3] == length[0]:
+#             return "Submarine"
+#         elif ship[3] == length[1]:
+#             return "Destroyer"
+#         elif ship[3] == length[2]:
+#             return "Cruiser"
+#         else:
+#             return "Battleship"
+#
+#
+# def is_open_sea(row, column, fleet):
+#     for ship in fleet:
+#         rows_squares = [ship[0]]
+#         column_squares = [ship[1]]
+#         if ship[2] == 'True':
+#             for i in range(ship[3]-1):
+#                 ship[1] += 1
+#                 column_squares.append(ship[1])
+#         else:
+#             for i in range(ship[3]-1):
+#                 ship[0] += 1
+#                 rows_squares.append(ship[0])
+#     if row in rows_squares or column in column_squares:
+#         return 'False'
+#     else:
+#         return 'True'
+#
 
 
-def ship():
-
-    row = random.randint(0,9)
-    column = random.randint(0,9)
-    horizontal = random.choice([True,False])
-    length = random.randint(1,4)
-    ships = tuple(np.array([row, column, horizontal, length]))
-
-    return ships
-
-f= ship()
-print(f)
+from numpy import zeros
+from random import choice, randint, shuffle
+# from functools import lru_cache
 
 
+SHIP_TYPES = {
+    1: "SUBMARINE",
+    2: "DESTROYER",
+    3: "CRUISER",
+    4: "BATTLESHIP"
+}
+
+ROW = 0
+COLUMN = 1
+HORIZONTAL = 2
+LENGTH = 3
+HITS = 4
+
+FLEET_TYPES = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1]
 
 
-
-
-
-
-
-
-
-def fleet
-
-
-
-def is_sunk(ship):
-    if len(ship[4]) == ship[3]:
-        return 'True'
-    else:
-        return 'False'
-
-
-#def squares_occupied(ship):
-
-
-
-
-def ship_type(ship):
-    length = [1, 2, 3, 4]
-    if ship[3] not in length:
-        return "wrong length"
-    else:
-        if ship[3] == length[0]:
-            return "Submarine"
-        elif ship[3] == length[1]:
-            return "Destroyer"
-        elif ship[3] == length[2]:
-            return "Cruiser"
-        else:
-            return "Battleship"
-
-
-def is_open_sea(row, column, fleet):
-    for ship in fleet:
-        rows_squares = [ship[0]]
-        column_squares = [ship[1]]
-        if ship[2] == 'True':
-            for i in range(ship[3]-1):
-                ship[1] += 1
-                column_squares.append(ship[1])
-        else:
-            for i in range(ship[3]-1):
-                ship[0] += 1
-                rows_squares.append(ship[0])
-    if row in rows_squares or column in column_squares:
-        return 'False'
-    else:
-        return 'True'
-
+def get_type(ship):
+    length = ship[LENGTH]
+    return SHIP_TYPES.get(length, "WRONG LENGTH")
 
 
 
