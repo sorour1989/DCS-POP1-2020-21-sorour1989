b679ce35b9c9757dca44e1bb6928fc77d9879c77
sorour1989 <64619099+sorour1989@users.noreply.github.com>
2021-01-09 01:40:09 +0000

    place_ship_at tested using parametrizing(raising error)

--- test_battleships.py
+++ test_battleships.py
@@ -60,13 +60,15 @@ def test_ok_to_place_ship_at(row, col, horizontal, length, fleet, result):
 -------
 
 
-def test_place_ship_at1():
-    ship1, ship2 = (2, 4, True, 2), (5, 6, True, 1)
-    fleet = [ship1, ship2]
-    new_ship = (7, 3, True, 4)
-    assert place_ship_at(new_ship, fleet) == [ship1, ship2, new_ship]
-    # add at least one test for place_ship_at by the deadline of session 7 assignment
-    # provide at least five tests in total for place_ship_at by the project submission deadline
+@pytest.mark.parametrize("row, col, horizontal, length, fleet, result",
+                         [
+                            (1, 4, True, 2, [(2, 4, True, 2, 0), (5, 6, True, 1, 0)], "Conflict detected.")
+
+                         ]
+                         )
+def test_place_ship_at(row, col, horizontal, length, fleet, result):
+    with pytest.raises(RuntimeError):
+        assert place_ship_at(row, col, horizontal, length, fleet) == result
 
 
 def test_check_if_hits1():
