b7a0073df8e67ac5aab9b6759da65a155a15631d
sorour1989 <64619099+sorour1989@users.noreply.github.com>
2021-01-10 00:45:37 +0000

    tests completed

--- test_battleships.py
+++ test_battleships.py
@@ -101,23 +101,32 @@ def test_place_ship_at_1(row, col, horizontal, length, fleet, result):
                          )
 def test_check_if_hits(row, col, fleet, result):
     assert check_if_hits(row, col, fleet) == result
----------------
-def test_hit1():
-    fleet = [
-        (1, 2, False, 3, {(1, 2), (2, 2), (3, 2)}),
-        (3, 0, False, 2, {(3, 0), (4, 0)}),
-             (5, 3, True, 2, {(5, 3), (5, 4)}), (8, 1, True, 2, {(8, 1), (8, 2)})]
-    assert hit(3, 2, fleet) == (
-        (1, 2, False, 3, {(1, 2), (2, 2), (3, 2)}), [(1, 2, False, 2, {(1, 2), (2, 2)}), (3, 0, False, 2, {(1, 2),
-                                                                                                           (4, 0)}),
-                                                     (5, 3, True, 2, {(5, 3), (5, 4)}),
-                                                     (8, 1, True, 2, {(8, 1), (8, 2)})])
-    # add at least one test for hit by the deadline of session 7 assignment
-    # provide at least five tests in total for hit by the project submission deadline
+
+@pytest.mark.parametrize("row, col, fleet, result",
+                         [
+                             (5, 6, [(2, 4, True, 2, 0), (5, 6, True, 1, 0)], ([(2, 4, True, 2, 0), (5, 6, True, 1, 1)], (5, 6, True, 1, 1))),
+                             (2, 0, [(1, 8, False, 3, 0), (1, 0, False, 2, 0), (4, 5, True, 1, 0), (0, 3, True, 3, 0)],
+                              ([(1, 8, False, 3, 0), (1, 0, False, 2, 1), (4, 5, True, 1, 0), (0, 3, True, 3, 0)], (1, 0, False, 2, 1))),
+                             (0, 0, [(4, 1, True, 2, 0), (0, 0, True, 1, 0), (2, 0, True, 4, 0)], ([(4, 1, True, 2, 0), (0, 0, True, 1, 1), (2, 0, True, 4, 0)], (0, 0, True, 1, 1))),
+                             (7, 8, [(2, 6, True, 1, 0), (7, 8, False, 1, 0)], ([(2, 6, True, 1, 0), (7, 8, False, 1, 1)], (7, 8, False, 1, 1))),
+                             (9, 3, [(9, 1, True, 4, 0), (8, 7, False, 2, 0), (7, 4, True, 2, 0), (5, 4, False, 1, 0)], ([(9, 1, True, 4, 1), (8, 7, False, 2, 0), (7, 4, True, 2, 0), (5, 4, False, 1, 0)], ((9, 1, True, 4, 1))))
 
 
-def test_are_unsunk_ships_left1():
-    fleet = [(1, 2, False, 3, {(1, 2), (2, 2), (3, 2)}), (8, 1, True, 2, {(8, 1), (8, 2)})]
-    assert are_unsunk_ships_left(fleet) is True
-    # add at least one test for are_unsunk_ships_left by the deadline of session 7 assignment
-    # provide at least five tests in total for are_unsunk_ships_left by the project submission deadline
+                         ]
+                         )
+def test_hit(row, col, fleet, result):
+    assert hit(row, col, fleet) == result
+
+
+
+@pytest.mark.parametrize("fleet, result",
+                         [
+                             ([(2, 1, True, 3, 0), (5, 1, False, 2, 2), (3, 5, True, 1, 0), (0, 6, True, 2, 1)], True),
+                             ([(8, 8, True, 2, 2), (6, 0, False, 4, 4), (5, 3, False, 1, 1), (5, 5, False, 1, 1)], False),
+                             ([(3, 9, False, 3, 1), (9, 0, True, 4, 2), (7, 0, True, 2, 0)], True),
+                             ([(1, 6, False, 2, 2), (6, 8, False, 1, 1), (1, 9, False, 3, 3)], False),
+                             ([(0, 1, False, 1, 1), (8, 2, False, 1, 0), (5, 5, False, 4, 3), (5, 2, False, 2, 2)], True)
+                         ]
+                         )
+def test_are_unsunk_ships_left(fleet, result):
+    assert are_unsunk_ships_left(fleet) is result
